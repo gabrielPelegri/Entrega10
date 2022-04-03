834fe531862e628dc14d618ffa612bec2befc3d3
vnavacar <vnavacar@myuax.com>
2022-04-03 19:21:42 +0200

    Ejercicio2 empezado

--- Entrega10/src/Ejercicio2.java
+++ Entrega10/src/Ejercicio2.java
@@ -9,5 +9,30 @@ public class Ejercicio2 {
 System.out.println("Introduzca un número entero");
 int a=0;
 
+int eleccion = 0;
+while (true) {
+    System.out.println("1: Sumar");
+    System.out.println("2: Multiplicar");
+    System.out.println("3: Dividir");
+    System.out.println("4: Finalizar programa");
+    try {
+        eleccion = teclado.nextInt();
+    } catch (InputMismatchException e) {
+        teclado.next();
+    }
+    switch (eleccion) {
+        case 1://sumar
+            
+            break;
+        case 2: //multiplicar
+            break;
+        case 3: //Dividir
+            break;
+        case 4:
+            System.exit(0);
+        default:
+            break;
+    }
+}
     }
 }
