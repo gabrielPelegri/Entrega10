a2a5fcd7e5ff09fa1ea8e8789863e6cf5117aef1
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-04-04 10:35:14 +0200

    cambios

--- Entrega10/src/Ejercicio2.java
+++ Entrega10/src/Ejercicio2.java
@@ -5,75 +5,7 @@ import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void Lanzar(Scanner teclado) {
+        Operacion.Operacion(teclado);
 
-        int eleccion = 0;
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        while (true) {
-            System.out.println("1: Sumar");
-            System.out.println("2: Multiplicar");
-            System.out.println("3: Dividir");
-            System.out.println("4: Finalizar programa");
-            try {
-                eleccion = teclado.nextInt();
-            } catch (InputMismatchException e) {
-                teclado.next();
-            }
-            switch (eleccion) {
-                case 1:// sumar
-                    System.out.println("Introduce un número entero");
-                    try {
-                        a = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    System.out.println("Introduce otro número");
-                    try {
-                        b = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    c = Operacion.Suma(a, b);
-                    System.out.println("Resultado: " + c);
-                    break;
-                case 2: // multiplicar
-                    System.out.println("Introduce un número");
-                    try {
-                        a = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    System.out.println("Introduce otro número");
-                    try {
-                        b = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    c = Operacion.Multiplicar(a, b);
-                    System.out.println("Resultado: " + c);
-                    break;
-                case 3: // Dividir
-                    System.out.println("Introduce un número");
-                    try {
-                        a = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    System.out.println("Introduce otro número");
-                    try {
-                        b = teclado.nextInt();
-                    } catch (InputMismatchException e) {
-                        teclado.next();
-                    }
-                    c = Operacion.Dividir(a, b);
-                    System.out.println("Resultado: " + c);
-                    break;
-                case 4:
-                    System.exit(0);
-                default:
-                    break;
-            }
-        }
     }
 }
--- Entrega10/src/Operacion.java
+++ Entrega10/src/Operacion.java
@@ -3,17 +3,72 @@ package Entrega10.src;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import javax.naming.ldap.SortKey;
+
 import Entrega10.src.excepciones.DesbordaCapacidadException;
 
 public class Operacion {
     public static void Operacion(Scanner teclado) {
+
         int a = 0;
-        try {
-            a = teclado.nextInt();
-        } catch (InputMismatchException e) {
+        int b = 0;
+        while (true) {
+            System.out.println("Introduzca un número entero");
+            try {
+                a = teclado.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                teclado.next();
+            }
+        }
+        if (a > 2147483647 || a < -2147483647) {
+            throw new DesbordaCapacidadException(
+                    "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
+        }
+        while (true) {
+            System.out.println("Introduzca otro número entero");
+            try {
+                b = teclado.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                teclado.next();
+            }
+        }
+        if (b > 2147483647 || b < -2147483647) {
+            throw new DesbordaCapacidadException(
+                    "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
+        }
+        String eleccion = "";
+        int resultado;
+        int salir = 0;
+        while (salir == 0) {
 
-        } catch (DesbordaCapacidadException e) {
-
+            menu();
+            eleccion = teclado.nextLine();
+            switch (eleccion) {
+                case "+":
+                    resultado = Suma(a, b);
+                    System.out.println("Solución: " + resultado);
+                    break;
+                case "-":
+                    resultado = Resta(a, b);
+                    System.out.println("Solución: " + resultado);
+                    break;
+                case "*":
+                    resultado = Multiplicar(a, b);
+                    System.out.println("Solución: " + resultado);
+                    break;
+                case "/":
+                    resultado = Dividir(a, b);
+                    System.out.println("Solución: " + resultado);
+                    break;
+                case "d":
+                    System.out.println("Saliendo...");
+                    salir = 1;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -27,7 +82,7 @@ public class Operacion {
         }
     }
 
-    public int Resta(int a, int b) {
+    public static int Resta(int a, int b) {
         if (a > 2147483647 || a < -2147483647 || b > 2147483647 || b < -2147483647) {
             throw new DesbordaCapacidadException(
                     "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
@@ -52,5 +107,9 @@ public class Operacion {
         } else {
             return a / b;
         }
+    }
+
+    public static void menu() {
+        System.out.println("+: suma | -:resta | *: multimplicación | /: división | d:salir");
     }
 }
