c3fa13ca23c350de57d066a571700ea1ebe918ad
gabrielPelegri <gabriel.pelegri.riis@gmail.com>
2022-04-04 20:37:30 +0200

    cambio a long

--- Entrega10/src/Ejercicio2.java
+++ Entrega10/src/Ejercicio2.java
@@ -4,9 +4,17 @@ package Entrega10.src;
 //no es utilizado
 import java.util.Scanner;
 
+import Entrega10.src.excepciones.DesbordaCapacidadException;
+
 public class Ejercicio2 {
     public static void Lanzar(Scanner teclado) {
-        Operacion.Operacion(teclado);
+        while (true) {
+            try {
+                Operacion.Operacion(teclado);
+            } catch (DesbordaCapacidadException e) {
+                System.out.println("Desbordamiento");
+            }
+        }
 
     }
 }
--- Entrega10/src/Operacion.java
+++ Entrega10/src/Operacion.java
@@ -11,12 +11,12 @@ import Entrega10.src.excepciones.DesbordaCapacidadException;
 public class Operacion {
     public static void Operacion(Scanner teclado) {
 
-        int a = 0;
-        int b = 0;
+        long a = 0;
+        long b = 0;
         while (true) {
             System.out.println("Introduzca un número entero");
             try {
-                a = teclado.nextInt();
+                a = teclado.nextLong();
                 break;
             } catch (InputMismatchException e) {
                 teclado.next();
@@ -29,7 +29,7 @@ public class Operacion {
         while (true) {
             System.out.println("Introduzca otro número entero");
             try {
-                b = teclado.nextInt();
+                b = teclado.nextLong();
                 break;
             } catch (InputMismatchException e) {
                 teclado.next();
@@ -51,26 +51,28 @@ public class Operacion {
                 case "+":
                     resultado = Suma(a, b);
                     System.out.println("Solución: " + resultado);
+                    salir = 1;
                     break;
                 case "-":
                     resultado = Resta(a, b);
                     System.out.println("Solución: " + resultado);
+                    salir = 1;
                     break;
                 case "*":
                     resultado = Multiplicar(a, b);
                     System.out.println("Solución: " + resultado);
                     break;
                 case "/":
-                try{
-                    resuldouble = Dividir(a, b);
-                    System.out.println("Solución: " + resuldouble);
-                }
-                    catch(ArithmeticException error){
+                    try {
+                        resuldouble = Dividir(a, b);
+                        System.out.println("Solución: " + resuldouble);
+                    } catch (ArithmeticException error) {
                         System.out.println("No se puede dividir por 0");
                     }
-                   // System.out.println("Solución: " + resuldouble);
+                    salir = 1;
+                    // System.out.println("Solución: " + resuldouble);
                     break;
-                case "d":
+                case "s":
                     System.out.println("Saliendo...");
                     salir = 1;
                     break;
@@ -80,51 +82,51 @@ public class Operacion {
         }
     }
 
-    public static int Suma(int a, int b) {
+    public static int Suma(long a, long b) {
         if (a > 2147483647 || a < -2147483647 || b > 2147483647 || b < -2147483647) {
             throw new DesbordaCapacidadException(
                     "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
         } else {
 
-            return a + b;
+            return (int) (a + b);
         }
     }
 
-    public static int Resta(int a, int b) {
+    public static int Resta(long a, long b) {
         if (a > 2147483647 || a < -2147483647 || b > 2147483647 || b < -2147483647) {
             throw new DesbordaCapacidadException(
                     "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
         } else {
-            return a - b;
+            return (int) (a - b);
         }
     }
 
-    public static int Multiplicar(int a, int b) {
+    public static int Multiplicar(long a, long b) {
         if (a > 2147483647 || a < -2147483647 || b > 2147483647 || b < -2147483647) {
             throw new DesbordaCapacidadException(
                     "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
         } else {
-            return a * b;
+            return (int) (a * b);
         }
     }
 
-    public static double Dividir(int a, int b) {
-      //  try{
+    public static double Dividir(long a, long b) {
+        // try{
         if (a > 2147483647 || a < -2147483647 || b > 2147483647 || b < -2147483647) {
             throw new DesbordaCapacidadException(
                     "El valor introducido desborda las capacidades de esta calculadora. Inténtelo de nuevo");
         } else {
-            //double returner = (double) a/b;
-            return (double) a/b;
+            // double returner = (double) a/b;
+            return (double) a / b;
         }
-  //  }
-  //      catch(ArithmeticException error){
-   //         System.out.println("No se puede dividir por 0");
-  //          return 0;
-   //     }
+        // }
+        // catch(ArithmeticException error){
+        // System.out.println("No se puede dividir por 0");
+        // return 0;
+        // }
     }
 
     public static void menu() {
-        System.out.println("+: suma | -:resta | *: multimplicación | /: división | d:salir");
+        System.out.println("+: suma | -:resta | *: multiplicación | /: división | d:salir");
     }
 }
